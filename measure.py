370a3cc9a66c3c9f44f72335a7cd371063255898
Noodles <noodles@noodles.services>
2024-11-26 00:57:35 +0100

    feat: organized code

--- measure.py
+++ measure.py
@@ -1,125 +1,185 @@
-"""
-Functions for measuring energy consumption
-
-Repeat the experiment 10 times for optimal results
-"""
 import json
-from uuid import uuid4
-
+from urllib.parse import urlparse
+import numpy as np
 from selenium.webdriver.firefox.webdriver import WebDriver as FirefoxWebDriver
 from codecarbon import OfflineEmissionsTracker
 from selenium import webdriver
+from datetime import datetime
 from typing import Literal
 import pandas as pd
 import subprocess
 import threading
-import datetime
 import os.path
 import atexit
 import time
+import os
 
-t_measurement = 10  # Measurement duration
-t_wait = 5  # Waiting time between measurements
-
+_TOOLS = Literal['scaphandre', 'codecarbon', 'powerjoular']
 driver: FirefoxWebDriver | None = None
-_TOOLS = Literal["scaphandre", "codecarbon", "powerjoular"]
-filename = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-tracker = OfflineEmissionsTracker | None
+tracker = OfflineEmissionsTracker()
+event = threading.Event()
 
 
-def measure_baseline_with(tool: _TOOLS, run_id):
-    global driver
-
-    time.sleep(t_wait)
-    print("Measuring OS...")
-    measure_with(tool, t_measurement, "OS", run_id)
-
-    driver = webdriver.Firefox()
-    time.sleep(t_wait)
-    print("Measuring Browser...")
-    measure_with(tool, t_measurement, "OS + BROWSER", run_id)
+def get_timestamp():
+    return datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 
 
-def measure_with(tool: _TOOLS, duration, url: str, run_id):
-    global tracker
+run_id = get_timestamp()
 
-    def codecarbon_end():
-        global driver, tracker
 
-        tracker.flush()
-        tracker.stop()
-        event.set()
+def get_domain(url):
+    return str(urlparse(url).netloc)
 
-        row = pd.read_csv('out/tmp/codecarbon.csv', nrows=1)
-        new_row = {
-            'id': run_id,
+
+def output_to_csv(row):
+    path = f'out/{run_id}.csv'
+    header = ('url,tool,timestamp,duration,energy,cpu_power,cpu_energy,gpu_power,gpu_energy,ram_power,'
+              'ram_energy,emissions,emissions_rate')
+
+    if os.path.isfile(path):
+        row.to_csv(path, mode='a', header=False, index=False)
+    else:
+        row.to_csv(path, header=header, index=False)
+
+
+def run_scaphandre(url, duration):
+    timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    domain = get_domain(url)
+
+    subprocess.run(["/usr/local/src/scaphandre/target/release/scaphandre", 'json', '-t', str(duration),
+                    '-s', '1', '-f', f'out/tmp/scaphandre-{domain}.json'])
+
+    with open(f'out/tmp/scaphandre-{domain}.json', 'r') as f:
+        raw = json.load(f)
+        consumption = []
+
+        for reading in raw:
+            consumption.append(reading['host']['consumption'])
+
+        mean_consumption = np.mean(consumption)
+
+        row = {
             'url': url,
-            'tool': tool,
-            'timestamp': row['timestamp'].values[0],
-            'duration': row['duration'].values[0],
-            'energy': row['energy_consumed'].values[0],
-            'cpu_power': row['cpu_power'].values[0],
-            'cpu_energy': row['cpu_energy'].values[0],
-            'gpu_power': row['gpu_power'].values[0],
-            'gpu_energy': row['gpu_power'].values[0],
-            'ram_power': row['ram_power'].values[0],
-            'ram_energy': row['ram_energy'].values[0],
-            'ram_size': row['ram_total_size'].values[0],
-            'emissions': row['emissions'].values[0],
-            'emissions_rate': row['emissions_rate'].values[0],
+            'tool': 'scaphandre',
+            'timestamp': timestamp,
+            'duration': float(duration),
+            'energy': mean_consumption / 10 ** 6,
         }
-        new_row_df = pd.DataFrame([new_row])
-        # outfile = 'out/out-' + str(run_id) + '.csv'
-        outfile = 'out/out-test.csv'
-        header = ('id,url,tool,timestamp,duration,energy,cpu_power,cpu_energy,gpu_power,gpu_energy,ram_power,'
-                  'ram_energy,ram_size,emissions,emissions_rate')
 
-        if os.path.isfile(outfile):
-            new_row_df.to_csv(outfile, mode='a', header=False, index=False)
-        else:
-            new_row_df.to_csv(outfile, header=header, index=False)
+        output_to_csv(pd.DataFrame([row]))
 
+
+def end_codecarbon(url, duration):
+    global driver, tracker
+    tracker.flush()
+    tracker.stop()
+    event.set()
+
+    tmp_row = pd.read_csv('out/tmp/codecarbon.csv', nrows=1)
+    row = {
+        'url': url,
+        'tool': 'codecarbon',
+        'timestamp': tmp_row['timestamp'].values[0],
+        'duration': tmp_row['duration'].values[0],
+        'energy': tmp_row['energy_consumed'].values[0],
+        'cpu_power': tmp_row['cpu_power'].values[0],
+        'cpu_energy': tmp_row['cpu_energy'].values[0],
+        'gpu_power': tmp_row['gpu_power'].values[0],
+        'gpu_energy': tmp_row['gpu_power'].values[0],
+        'ram_power': tmp_row['ram_power'].values[0],
+        'ram_energy': tmp_row['ram_energy'].values[0],
+        'emissions': tmp_row['emissions'].values[0],
+        'emissions_rate': tmp_row['emissions_rate'].values[0],
+    }
+
+    output_to_csv(pd.DataFrame([row]))
+
+    try:
         os.remove('out/tmp/codecarbon.csv')
-        tracker = OfflineEmissionsTracker(measure_power_secs=t_measurement)
-        subprocess.run("rm", "/tmp/.codecarbon.lock")
-
-    match tool:
-        case "scaphandre":
-            subprocess.run(
-                ["scaphandre", "json", "-t", str(duration), '-f', 'out/tmp/scaphandre-' + filename + '.json'],
-                capture_output=True)
-            print("Done Scaphandre")
-
-        case "codecarbon":
-            tracker.start()
-            event = threading.Event()
-            threading.Timer(t_measurement, codecarbon_end).start()
-            event.wait()
+        tracker = OfflineEmissionsTracker(measure_power_secs=duration)
+        subprocess.run(['rm', '/tmp/.codecarbon.lock'])
+    except Exception as e:
+        print(e)
 
 
-def run_experiment(tool: _TOOLS):
-    if tool == "codecarbon":
-        tracker = OfflineEmissionsTracker(measure_power_secs=t_measurement)
-        atexit.register(tracker.stop)
-
-    run_id = uuid4()
-    measure_baseline_with(tool, run_id)
-
-    with open('data/test.json', 'r') as file:
-        urls = json.load(file)
-
-        for url in urls:
-            try:
-                print("Loading " + url + "...")
-                driver.get(url)
-                time.sleep(t_wait)  # Wait with tracking to calibrate energy readings
-                measure_with(tool, t_measurement, url, run_id)
-            except Exception:
-                print("FAILED TO LOAD " + url)
-
-        file.close()
-
-    driver.quit()
+def run_codecarbon(url, duration):
+    tracker.start()
+    threading.Timer(duration, end_codecarbon, args=[url, duration]).start()
+    event.wait()
 
 
-run_experiment("scaphandre")
+# FIXME: Powerjoular requires root privileges; Test on a different system.
+def run_powerjoular(url, duration):
+    domain = get_domain(url)
+    path = f'out/tmp/powerjoular-{domain}.csv'
+    process = subprocess.Popen([
+        'sudo', '/usr/local/src/powerjoular/obj/powerjoular', '-f', path])
+    time.sleep(duration)
+    process.terminate()
+
+    with open(path, 'r') as f:
+        tmp_row = pd.read_csv(path, nrows=1)
+
+        row = {
+            'url': url,
+            'tool': 'powerjoular',
+            'timestamp': tmp_row['timestamp'].values[0],
+            'duration': float(duration),
+            'energy': tmp_row['Total Power'] / duration,
+            'cpu_power': tmp_row['CPU Power'],
+            'cpu_energy': '',
+            'gpu_power': tmp_row['GPU Power']
+        }
+
+        output_to_csv(pd.DataFrame([row]))
+
+
+def measure(url, duration, delay, tool: _TOOLS):
+    print(f"\nLoading {url}...")
+
+    try:
+        if url != 'os':
+            driver.get(url)
+
+        time.sleep(delay)
+
+        match tool:
+            case "scaphandre":
+                run_scaphandre(url, duration)
+            case "codecarbon":
+                run_codecarbon(url, duration)
+            case "powerjoular":
+                run_powerjoular(url, duration)
+
+    except Exception as e:
+        print(f"Failed to load {url}:\n{str(e)}")
+
+
+def main():
+    global driver, tracker, run_id
+
+    tool: _TOOLS = 'scaphandre'
+    duration = 10  # Measurement duration
+    delay = 5  # Delay between measurements
+    repeat = 1
+    atexit.register(tracker.stop)
+
+    for i in range(repeat):
+        run_id = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+
+        # Measure baseline
+        measure("os", duration, delay, tool)
+        driver = webdriver.Firefox()
+        measure("about:blank", duration, delay, tool)
+
+        # Measure URLs
+        with open('data/test.json', 'r') as f:
+            urls = json.load(f)
+            for url in urls:
+                measure(url, duration, delay, 'codecarbon')
+
+        driver.quit()
+
+
+if __name__ == '__main__':
+    main()
